daa6d21bfc87a27a004b95fa4708fe4cbccdae5e
Kai Brandes <kai.brandes@otto.de>
2015-04-01 15:28:13 +0200

    fix for logging issues

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-cachefile "0.0.1"
+(defproject de.otto/tesla-cachefile "0.0.2"
   :description "Addon to https://github.com/otto-de/tesla-microservice to use a cachefile locally or on hdfs."
   :url "https://github.com/otto-de/tesla-cachefile"
   :license {:name "Apache License 2.0"
@@ -10,10 +10,13 @@
                  [de.otto/tesla-microservice "0.1.10"]
                  [de.otto/tesla-zookeeper-observer "0.1.0"]
 
+                 [org.slf4j/slf4j-api "1.7.7"]
+                 [org.slf4j/slf4j-jcl "1.7.7"]
                  [org.apache.hadoop/hadoop-hdfs "2.2.0"
                   :exclusions [[javax.servlet.jsp/jsp-api]
                                [javax.servlet/servlet-api]
-                               [tomcat/jasper-runtime]]]
+                               [tomcat/jasper-runtime]
+                               [commons-logging/commons-logging]]]
 
                  [org.apache.hadoop/hadoop-common "2.2.0"
                   :exclusions [[org.slf4j/slf4j-log4j12]
