8b66f19d2d9076cd53a9f768963ae75a703e7754
Kai Brandes <kai.brandes@otto.de>
2015-11-18 15:51:41 +0100

    move logging of tk-namenode injection to debug

--- src/de/otto/tesla/cachefile/utils/zk_namenode.clj
+++ src/de/otto/tesla/cachefile/utils/zk_namenode.clj
@@ -11,7 +11,7 @@
 
 (defn- namenode-from-zookeeper [zk]
   (let [path "/hadoop-ha/hadoop-ha/ActiveBreadCrumb"]
-    (log/info "choosing zookeeper to determine namenode. Zk-path: " path)
+    (log/debug "choosing zookeeper to determine namenode. Zk-path: " path)
     (zk/observe! zk path parse-hostname)))
 
 (defn- replace-namenode-placholder [path zookeeper]
