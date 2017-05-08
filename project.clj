0c5e9b0bb4a5732997cda2cbbaf96acef20e4636
tesla <tesla@dv.otto.de>
2017-05-08 13:16:01 +0200

    cd| do not leak logging implementations

--- project.clj
+++ project.clj
@@ -7,14 +7,17 @@
         :url  "https://github.com/otto-de/tesla-cachefile"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.stuartsierra/component "0.3.2"]
-                 [de.otto/tesla-zookeeper-observer "0.1.5"]
+                 [de.otto/tesla-zookeeper-observer "0.1.5"
+                  :exclusions [log4j/log4j]]
                  [overtone/at-at "1.2.0"]
                  [metrics-clojure "2.7.0"]
                  [org.slf4j/slf4j-api "1.7.21"]
                  [org.clojure/core.async "0.2.395"]
                  [org.slf4j/jcl-over-slf4j "1.7.21"]
-                 [org.apache.hadoop/hadoop-common "2.6.0"]
-                 [org.apache.hadoop/hadoop-hdfs "2.6.0"]
+                 [org.apache.hadoop/hadoop-common "2.6.0"
+                  :exclusions [commons-logging/commons-logging log4j/log4j org.slf4j/slf4j-log4j12]]
+                 [org.apache.hadoop/hadoop-hdfs "2.6.0"
+                  :exclusions [commons-logging/commons-logging log4j/log4j org.slf4j/slf4j-log4j12]]
                  [hdfs-clj "0.1.15"]]
   :target-path "target/%s"
   :lein-release {:deploy-via :clojars}
