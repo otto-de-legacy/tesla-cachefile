1b0ab167202b19b844c59c49abf5b12dbb2b1f4c
Carl Düvel <carl.duevel@otto.de>
2018-02-12 13:28:55 +0100

    Dependency updates

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url  "http://www.apache.org/license/LICENSE-2.0.html"}
   :scm {:name "git"
         :url  "https://github.com/otto-de/tesla-cachefile"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.stuartsierra/component "0.3.2"]
                  [de.otto/tesla-zookeeper-observer "0.2.1"
                   :exclusions [log4j/log4j]]
                  [overtone/at-at "1.2.0"]
                  [metrics-clojure "2.10.0"]
                  [org.slf4j/slf4j-api "1.7.25"]
-                 [org.clojure/core.async "0.3.465"]
+                 [org.clojure/core.async "0.4.474"]
                  [org.slf4j/jcl-over-slf4j "1.7.25"]
                  [org.apache.hadoop/hadoop-common "2.9.0"
                   :exclusions [commons-logging/commons-logging log4j/log4j org.slf4j/slf4j-log4j12 org.mortbay.jetty/jetty org.mortbay.jetty/jetty-util org.mortbay.jetty/jetty-sslengine]]
@@ -29,9 +29,9 @@
   :target-path "target/%s"
   :lein-release {:deploy-via :clojars}
   :test-paths ["test" "test-resources"]
-  :profiles {:dev     {:dependencies [[de.otto/tesla-microservice "0.11.16"]
+  :profiles {:dev     {:dependencies [[de.otto/tesla-microservice "0.11.25"]
                                       [ch.qos.logback/logback-core "1.2.3"]
                                       [ch.qos.logback/logback-classic "1.2.3"]]
 
-                       :plugins      [[lein-ancient "0.5.4"] [lein-release/lein-release "1.0.9"]]}
+                       :plugins      [[lein-release/lein-release "1.0.9"]]}
              :uberjar {:aot :all}})
